d7fd6639ef530c2684059e1c1c043a1a196e413d
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2020-12-10 13:06:59 +0100

    end of task 1.1

--- src/main/java/de/uniba/dsam/task1/controller/MovieController.java
+++ src/main/java/de/uniba/dsam/task1/controller/MovieController.java
@@ -19,6 +19,7 @@ public class MovieController {
         // init movie list
         this.movies.add(new Movie("1", "Inception", false, 2010, "https://cdn.pixabay.com/photo/2017/05/15/17/43/calm-2315559_960_720.jpg"));
         this.movies.add(new Movie("2", "Cloud Atlas", false, 2012, "https://cdn.pixabay.com/photo/2020/03/02/16/19/vintage-4896141_960_720.jpg"));
+        this.movies.add(new Movie());
     }
 
     @GetMapping
--- src/main/java/de/uniba/dsam/task1/model/Movie.java
+++ src/main/java/de/uniba/dsam/task1/model/Movie.java
@@ -2,9 +2,11 @@ package de.uniba.dsam.task1.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     private String id;
     private String title;
